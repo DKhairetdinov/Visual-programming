eb5ac6e9673e785685cccaeb977628e51cc25836
DKhairetdinov <haretdinov.201@mail.ru>
2026-03-17 07:59:52 +0400

    Complete lab6

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,20 +1,21 @@
 import { describe, it, expectTypeOf } from 'vitest';
-import { DeepReadonly } from "./index.js"
+import { DeepReadonly, PickedByType, EventHandlers } from './index.js';
 
-describe('Lab - 6 Utility type', () => {
-  it('1. DeepReadonly recursion work', () => {
+describe('Lab 6 - Utility Types', () => {
+
+  it('1. DeepReadonly work recursion', () => {
     type Config = {
       api: {
-        port : {
-          settings : {
-            debug : boolean;
-          }
-        }
-      }
-    }
+        port: number;
+        settings: {
+          debug: boolean;
+        };
+      };
+    };
 
     type ReadonlyConfig = DeepReadonly<Config>;
 
+    // Проверяем, что верхний уровень readonly
     expectTypeOf<ReadonlyConfig>().toEqualTypeOf<{
       readonly api: {
         readonly port: number;
@@ -25,4 +26,37 @@ describe('Lab - 6 Utility type', () => {
     }>();
   });
 
-}); 
+  it('2. PickedByType filter properties by type', () => {
+    type User = {
+      id: number;
+      name: string;
+      age: number;
+      email: string;
+      isActive: boolean;
+    };
+
+    type OnlyStrings = PickedByType<User, string>;
+
+    expectTypeOf<OnlyStrings>().toEqualTypeOf<{
+      name: string;
+      email: string;
+    }>();
+
+    expectTypeOf<OnlyStrings>().not.toHaveProperty('id');
+    expectTypeOf<OnlyStrings>().not.toHaveProperty('isActive');
+  });
+
+  it('3. EventHandlers generate name onClick, onHover etc.', () => {
+    type MyEvents = {
+      click: { x: number; y: number };
+      focus: { target: string };
+    };
+
+    type Handlers = EventHandlers<MyEvents>;
+
+    expectTypeOf<Handlers>().toEqualTypeOf<{
+      onClick: (event: { x: number; y: number }) => void;
+      onFocus: (event: { target: string }) => void;
+    }>();
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,11 @@ export type DeepReadonly<T> = {
     : DeepReadonly<T[K]>
   :T[K];
 };
+
+export type PickedByType<T, U> = {
+  [K in keyof T as T[K] extends U ? K : never]: T[K];
+};
+
+export type EventHandlers<T> = {
+  [K in keyof T as K extends string ? `on${Capitalize<K>}` : never]: (event: T[K]) => void;
+};
